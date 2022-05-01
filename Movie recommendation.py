1b3e9e64097bbde2cfa37e1e424c2a956a716444
Rishabh <rishabhkaushal1796@gmail.com>
2022-05-01 22:57:00 +0530

    Dataset added

--- Movie recommendation.py
+++ Movie recommendation.py
@@ -1,4 +1,9 @@
 import numpy as np
 import pandas as pd
 import warnings
-warnings
+warnings.filterwarnings('ignore')
+
+#get the dataset
+df = pd.read_csv("ml-100k/u.data", sep ='\t') #seperator is /t - tab
+print(df.head())
+print(df.shape) 
