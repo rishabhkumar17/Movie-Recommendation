cab9d3cc487dcca7dfeca013c6ee57e1256dfbc1
Rishabh <rishabhkaushal1796@gmail.com>
2022-05-02 22:33:40 +0530

    Exploratory data analysis

--- Movie recommendation.py
+++ Movie recommendation.py
@@ -7,8 +7,28 @@ warnings.filterwarnings('ignore')
 
 columns_name = ["user_id","item_id", "rating", "timestamp"]
 df = pd.read_csv("ml-100k/u.data", sep ='\t', names = columns_name) #seperator is /t - tab
-print(df.head())
-print(df.shape) 
 
 movies_title = pd.read_csv("ml-100k/u.item", sep ='\|', header = None , encoding = "ISO-8859-1")
-print(movies_title)
+
+movies_title = movies_title[[0,1]]
+movies_title.columns = ["item_id", "title"]
+df = pd.merge(df, movies_title, on="item_id")
+
+# Exploratory data analysis
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('white')
+
+df.groupby('title').mean()['rating'].sort_values(ascending=False)
+df.groupby('title').count()['rating'].sort_values(ascending=False)
+
+ratings = pd.DataFrame(df.groupby('title').mean()['rating'])
+
+ratings['num of ratings'] = pd.DataFrame(df.groupby('title').count()['rating'])
+
+#plt.figure(figsize=(10,6))
+#plt.hist(ratings['num of ratings'], bins = 70)
+
+sns.jointplot(x='rating',y='num of ratings',data=ratings,alpha=0.5)
+plt.show()
