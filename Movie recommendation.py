4c73f3701dda433aacc42d380659f00f0b04f818
Rishabh <rishabhkaushal1796@gmail.com>
2022-05-02 21:51:57 +0530

    Movie title dataset added

--- Movie recommendation.py
+++ Movie recommendation.py
@@ -3,7 +3,12 @@ import pandas as pd
 import warnings
 warnings.filterwarnings('ignore')
 
-#get the dataset
-df = pd.read_csv("ml-100k/u.data", sep ='\t') #seperator is /t - tab
+#Get the dataset
+
+columns_name = ["user_id","item_id", "rating", "timestamp"]
+df = pd.read_csv("ml-100k/u.data", sep ='\t', names = columns_name) #seperator is /t - tab
 print(df.head())
 print(df.shape) 
+
+movies_title = pd.read_csv("ml-100k/u.item", sep ='\|', header = None , encoding = "ISO-8859-1")
+print(movies_title)
